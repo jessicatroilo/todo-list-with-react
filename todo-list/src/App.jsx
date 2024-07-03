486e0b1fe75b31c5285f00681f65dd043b50036b
jessicatroilo <jessica.troilo@yahoo.fr>
2024-07-03 10:48:27 +0200

    install vite and react

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  
+  //1. le state 
+
+  //2. Le comportement
+
+  //3. Le rendu
+  return (
+    <>
+    <h1> coucou </h1>
+    </>
+  )
+}
+
+export default App
